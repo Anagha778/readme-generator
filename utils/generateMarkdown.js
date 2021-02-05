5a71e15c00b168057726364a6805df04e7d935ed
anagha778 <anagha778@gmail.com>
2021-02-04 22:26:58 -0500

    Added licenselink function code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,7 +14,28 @@ const renderLicenseBadge = license => {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+const renderLicenseLink = license => {
+    if (license === "MIT")
+    {
+      return `https://choosealicense.com/licenses/mit/`;
+    }
+    else if (license === "Apache")
+    {
+      return `https://choosealicense.com/licenses/apache-2.0/`;
+    }
+    else if (license === "GPLv3")
+    {
+      return `https://choosealicense.com/licenses/gpl-3.0/`;
+    }
+    else if (license === "Unlicense")
+    {
+      return `https://choosealicense.com/licenses/unlicense/`;
+    }
+    else 
+    {
+      return ``;
+    }
+  };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
