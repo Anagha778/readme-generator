347634bf927a6ff84f8f0abffec1fd5afc37d205
anagha778 <anagha778@gmail.com>
2021-02-04 22:24:10 -0500

    Added generateMarkdown function code

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,9 +11,31 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-`;
+const generateMarkdown = data => {
+    return `# ${data.pname} ![Github license](${renderLicenseBadge(data.plicense)})
+    ## Description
+    ${data.pdescription}
+    ## Table of Contents:
+    * [Installation](#Installation)
+    * [Usage](#Usage)
+    * [License](#License)
+    * [Contributor](#Contributors)
+    * [Tests](#Tests)
+    * [Questions](#Questions)
+    ## Installation:
+    Install dependency using command : ${data.pdependency}
+    ## Usage:
+    ${data.pusage}
+    ## License:
+    ${renderLicenseSection(data.plicense)}
+    ## Contributors:
+    ${data.pcontributor}
+    ## Tests:
+    ${data.ptest}
+    ## Questions:
+    * Link to my GitHub profile : https://github.com/${data.username}  
+    * For additional questions reach me at : ${data.eaddress}
+    `;
 }
 
 module.exports = generateMarkdown;
