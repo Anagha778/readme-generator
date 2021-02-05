d943e1201e8f9c451f9e281c0f89ac7c16233505
anagha778 <anagha778@gmail.com>
2021-02-04 22:19:16 -0500

    Added questions array

--- index.js
+++ index.js
@@ -1,8 +1,93 @@
-  
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require("inquirer");
+const filepath = './dist/readme.md';
+const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your GitHub username (Required) : ',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your GitHub username!');
+          return false;
+        }
+      }
+    },
+    {
+        type: 'input',
+        name: 'eaddress',
+        message: 'What is your email address (Required) : ',
+        validate: addressInput => {
+            if (addressInput) {
+              return true;
+            } else {
+              console.log('Please enter your email address!');
+              return false;
+            }
+          }
+    },
+    {
+        type: 'input',
+        name: 'pname',
+        message: 'What is your project name (Required) : ',
+        validate: projectInput => {
+            if (projectInput) {
+              return true;
+            } else {
+              console.log('Please enter your project name!');
+              return false;
+            }
+          }
+    },
+    {
+        type: 'input',
+        name: 'pdescription',
+        message: 'Please provide decription of your project (Required) : ',
+        validate: descInput => {
+            if (descInput) {
+              return true;
+            } else {
+              console.log('Please provide project description!');
+              return false;
+            }
+          }
+    },
+    {
+        type: 'input',
+        name: 'pdependency',
+        message: 'Please provide command to install your project dependencies: '
+    },
+    {
+        type: 'input',
+        name: 'pusage',
+        message: 'Please provide usage information for your project: '
+    },
+    {
+        type: 'input',
+        name: 'pcontributor',
+        message: 'Please provide Contributor Covenant Code of Conduct: '
+    },
+    {
+        type: 'input',
+        name: 'ptest',
+        message: 'Please provide tests executed for this project as part of testing: '
+    },
+    {
+        type: 'list',
+        name: 'plicense',
+        message: 'Please choose license from below list: ',
+        choices:['MIT',
+                'Apache',
+                'GPLv3',
+                'Unlicense'],
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
